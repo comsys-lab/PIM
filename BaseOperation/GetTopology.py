2f13e025fc31864f31fd41243bdfedab12133458
Seong-Jun-Park <tank7113@korea.ac.kr>
2023-08-04 16:00:52 +0900

    .

--- BaseOperation/GetTopology.py
+++ BaseOperation/GetTopology.py
@@ -1,32 +1,68 @@
 import numpy as np
 
 class GetTopology:
-    #return topology layer length
-    def return_layer_length(self, path):
-        with open(path, 'r') as file:
-            return sum(1 for _ in file)
+    def __init__(self):
+        self.topo = []
+        self.MNK = []
 
+    def GetTopology(self, path):
+        topo, MNK, len_one = self.read_csv(path)
+
+        #If topology file is entered with Original format, we have to convert it into MNK format.
+        #If topology file is entered with MNK format, we have to convert it into Original format.
+        if MNK:
+            self.MNK = topo
+            self.Change_MNK_to_Original(self.MNK, len_one)
+        else:
+            self.topo = topo
+            self.Change_Original_to_MNK(self.topo,len_one)
+    
+        return self.topo, self.MNK
+    
     #read files from topology file
-    def read_files(self, path):
+    def read_csv(self, path):
         topology = np.loadtxt(path, delimiter=',', usecols=np.arange(1,8), dtype=int)
 
+        #Check Layer length (topology length)
         length = int(len(topology))
-
+        
         if length == 1:
-            return [topology.tolist()]
+            topo = [topology.tolist()]
+            MNK = len(topo) == 4
+            len_one = True
         else:
-            return topology.tolist()
+            topo = topology.tolist()
+            MNK = len(topo[0]) == 4
+            len_one = False
+        
+        return topo, MNK, len_one
+    
+    def Change_Original_to_MNK(self, topo_all, len_one):
+        if len_one:
+            self.MNK.append(self.Change_Original_to_MNK_One_Layer(topo_all))
+        else:
+            for topo in topo_all:
+                self.MNK.append(self.Change_Original_to_MNK_One_Layer(topo))
 
-    #Make MNK and Check, integrated
-    def process_topology(self, topology):
-        result = []
-        for i in topology:
-            M = int(np.ceil((i[0] - i[2] + i[6]) / i[6]) * np.ceil((i[1] - i[3] + i[6]) / i[6]))
-            N = i[2] * i[3] * i[4]
-            K = i[5]
-            result.append([M, N, K])
-        return result
+    def Change_Original_to_MNK_One_Layer(self, topo):
+        #Topology is composed of ['run_name',input_width, input_height, filter_width, filter_height,
+        #channel, number_of_fiter, stride]
+        M = int(np.ceil((topo[1]-topo[3]+topo[7])/topo[7])*np.ceil((topo[2]-topo[4]+topo[7])/topo[7]))
+        N = topo[3] * topo[4] * topo[5]
+        K = topo[6]
+        MNK = [topo[0],M,N,K]
 
-    def return_topology(self,path):
-        topology = self.read_files(path)
-        return topology, self.process_topology(topology)
+        return MNK
+
+    def Change_MNK_to_Original(self, MNK_all, len_one):
+        if len_one:
+            self.topo.append(self.Change_MNK_to_Original_One_Layer(MNK_all))
+        else:
+            for MNK in MNK_all:
+                self.topo.append(self.Change_MNK_to_Original_One_Layer(MNK))
+
+    def Change_MNK_to_Original_One_Layer(self, MNK):
+        #MNK is composed of ['run_name',M,N,K]
+        topo = [MNK[0],MNK[1],1,1,1,MNK[2],MNK[3],1]
+
+        return topo
--- config.py
+++ config.py
@@ -44,85 +44,102 @@ class config:
 
     def read_config_file(self, config_file):
         
-        config = cp.configparser()
+        config = cp.ConfigParser()
         config.read(config_file)
-
+        
+        #In this section, we distribute form factor with two types; Mobile and PC (Throughput), Server and Supercomputer.
         section = 'Form_Factor'
         self.Form_Factor = config.get(section, 'Form_Factor')
-        if self.Form_Factor == ('Mobile' or 'PC'):
+        if (self.Form_Factor == 'Mobile') or (self.Form_Factor == 'PC'):
             self.NPU_flag = True
         else:
             self.NPU_flag = False
 
+        #In this section, we enter parameters for NPUs.
         section = 'NPU_Parameters'
+
+        self.NPU_Pod_Dimension_Row = config.getint(section, 'NPU_Pod_Dimension_Row')
+        self.NPU_Pod_Dimension_Col = config.getint(section, 'NPU_Pod_Dimension_Col')
+        self.NPU_Number_of_Pod = config.getint(section, 'NPU_Number_of_Pod')
+        self.NPU_Input_Buffer = config.getint(section, 'NPU_Input_Buffer')
+        self.NPU_Filter_Buffer = config.getint(section, 'NPU_Filter_Buffer')
+        self.NPU_Clock_Frequency = config.getfloat(section, 'NPU_Clock_Frequency')
+        self.NPU_Total_Bandwidth = config.getfloat(section, 'NPU_Total_Bandwidth')
+
         if self.NPU_flag == True:
-            self.NPU_Throughput = float(config.get(section, 'Throuhgput'))
-            self.NPU_Systolic_Row, self.NPU_Systolic_Col = self.convert_throughput(self.NPU_Throughput,\
-                 self.NPU_Pod_Dimension_Row, self.NPU_Pod_Dimension_Col, self.NPU_Clock_Frequency)
+            self.NPU_Throughput = config.getfloat(section, 'NPU_Throughput')
+            self.NPU_Systolic_Row, self.NPU_Systolic_Col, self.NPU_Pod_Dimension_Row, self.NPU_Pod_Dimension_Col\
+                  = self.convert_throughput(self.NPU_Throughput, self.NPU_Pod_Dimension_Row, \
+                                            self.NPU_Pod_Dimension_Col, self.NPU_Clock_Frequency)
         
         else:
-            self.NPU_Systolic_Row = int(config.get(section, 'Systolic_Row'))
-            self.NPU_Systolic_Col = int(config.get(section, 'Systolic_Col'))
-
-        self.NPU_Pod_Dimension_Row = int(config.get(section, 'Pod_Dimension_Row'))
-        self.NPU_Pod_Dimension_Col = int(config.get(section, 'Pod_Dimension_Col'))
-        self.NPU_Number_of_Pod = int(config.get(section, 'Number_of_Pod'))
-        self.NPU_Input_Buffer = int(config.get(section, 'Input_Buffer'))
-        self.NPU_Filter_Buffer = int(config.get(section, 'Filter_Buffer'))
-        self.NPU_Clock_Frequency = int(config.get(section, 'Clock_Frequency'))
-        self.NPU_Total_Bandwidth = float(config.get(section, 'Total_Bandwidth'))
-
+            self.NPU_Systolic_Row = config.getint(section, 'NPU_Systolic_Row')
+            self.NPU_Systolic_Col = config.getint(section, 'NPU_Systolic_Col')
+        
+        #In this section, enter the parameters for PIM units.
         section = 'PIM_Parameters'
-        self.Systolic_Row = int(config.get(section, 'Systolic_Row'))
-        self.Systolic_Col = int(config.get(section, 'Systolic_Col'))
-        self.Pod_Dimension_Row = int(config.get(section, 'Pod_Dimension_Row'))
-        self.Pod_Dimension_Col = int(config.get(section, 'Pod_Dimension_Col'))
-        self.PU_per_Chip = int(config.get(section, 'PU_per_Chip'))
-        self.Chip_per_DIMM = int(config.get(section, 'Chip_per_DIMM'))
-        self.Number_of_DIMM = int(config.get(section, 'NUmber_of_DIMM'))
-        self.Input_Buffer = int(config.get(section, 'Input_Buffer'))
-        self.Filter_Buffer = int(config.get(section, 'Filter_Buffer'))
-        self.PIM_Bandwidth_per_DIMM = float(config.get(section, 'PIM_Bandwidth_per_DIMM'))
+        self.PIM_Systolic_Row = config.getint(section, 'PIM_Systolic_Row')
+        self.PIM_Systolic_Col = config.getint(section, 'PIM_Systolic_Col')
+        self.PIM_Pod_Dimension_Row = config.getint(section, 'PIM_Pod_Dimension_Row')
+        self.PIM_Pod_Dimension_Col = config.getint(section, 'PIM_Pod_Dimension_Col')
+        self.PU_per_Chip = config.getint(section, 'PU_per_Chip')
+        self.Chip_per_DIMM = config.getint(section, 'Chip_per_DIMM')
+        self.Number_of_DIMM = config.getint(section, 'NUmber_of_DIMM')
+        self.PIM_Input_Buffer = config.getint(section, 'PIM_Input_Buffer')
+        self.PIM_Filter_Buffer = config.getint(section, 'PIM_Filter_Buffer')
+        self.PIM_Clock_Frequency = config.getint(section, 'PIM_Clock_Frequency')
+        self.PIM_Bandwidth_per_DIMM = config.getfloat(section, 'PIM_Bandwidth_per_DIMM')
 
+        #In this section, enter the parameters for DNN Models. 
         section = 'DNN_Parameters'
         self.Topology_Path = config.get(section, 'Topology_Path')
-        self.Batch = int(config.get(section, 'Batch'))
+        self.Batch = config.getint(section, 'Batch')
         self.NPU_Dataflow = config.get(section, 'NPU_Dataflow')
         self.PIM_Dataflow = config.get(section, 'PIM_Dataflow')
 
-        section = 'Save_parameters'
-        self.PIM_Flag = eval(config.get(section, 'PIM_Flag'))
-        self.Storing_Path = int(config.get(section, 'Storing_Path'))
-
-
+        #In this section, eneter the parameters for saving results.
+        section = 'Save_Parameters'
+        self.PIM_Flag = config.getboolean(section, 'PIM_Flag')
+        self.Storing_Path = config.get(section, 'Storing_Path')
         
     def convert_throughput(self, throughput, NPU_Pod_Dimension_Row, NPU_Pod_Dimension_Col, clock_frequency):
         #Throughput = 2 * # of pod * clock frequency
         #of pod = Throughput / (2 * clock frequency)
 
-        num_pe = throughput /(2 * clock_frequency)
+        num_pe = throughput * 1024 /(2 * clock_frequency)
         mul_two = int(round(math.log(num_pe) / math.log(2),0))
-        
-        if mul_two//2 == 0:
+        print(mul_two)
+        if mul_two % 2 == 0:
             row = pow(2,int(mul_two/2))
             col = pow(2,int(mul_two/2))
         else:
             row = pow(2,int(mul_two/2))
             col = pow(2,int(mul_two/2)+1)
         #For row case
-        if row // NPU_Pod_Dimension_Row == 0:
-            row_dim = int(row / NPU_Pod_Dimension_Row)
+        if row % NPU_Pod_Dimension_Row == 0:
+            row_dim =  NPU_Pod_Dimension_Row
             row = int(row / row_dim)
         else:
             row_dim = 1
 
         #For col case
-        if col // NPU_Pod_Dimension_Col == 0:
-            col_dim = int(col / NPU_Pod_Dimension_Col)
+        if col % NPU_Pod_Dimension_Col == 0:
+            col_dim = NPU_Pod_Dimension_Col
             col = int(col / col_dim)
         else:
             col_dim = 1
 
         return row, col, row_dim, col_dim
 
-        
+    def get_parameters(self):
+        npu_params = [self.NPU_Systolic_Row, self.NPU_Systolic_Col, self.NPU_Pod_Dimension_Row, \
+                            self.NPU_Pod_Dimension_Col, self.NPU_Number_of_Pod, self.NPU_Input_Buffer, \
+                            self.NPU_Filter_Buffer, self.NPU_Clock_Frequency, self.NPU_Total_Bandwidth]
+        pim_params = [self.PIM_Systolic_Row, self.PIM_Systolic_Col, self.PIM_Pod_Dimension_Row, \
+                           self.PIM_Pod_Dimension_Col, self.PU_per_Chip, self.Chip_per_DIMM, \
+                            self.Number_of_DIMM, self.PIM_Input_Buffer, self.PIM_Filter_Buffer, \
+                                self.PIM_Clock_Frequency, self.PIM_Bandwidth_per_DIMM]
+        dnn_params = [self.Topology_Path, self.Batch, self.NPU_Dataflow, self.PIM_Dataflow]
+        save_params = [self.PIM_Flag, self.Storing_Path]
+
+        return npu_params, pim_params, dnn_params, save_params
